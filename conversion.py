0e6ef5f8909d790144f9dedd1a71d75449c60905
ajnisbet <ajnisbet@gmail.com>
2024-12-02 14:33:11 -0800

    Finish mod 3

---
+++ conversion.py
@@ -0,0 +1,12 @@
+ONE_DAY_IN_SECONDS = 86_400
+
+
+def acre_feet_to_m3(volume_acre_feet):
+    """Converts volume in US acre-feet to SI m³."""
+    volume_m3 = volume_acre_feet * 1_233.482
+    return volume_m3
+
+def cms_to_cfs(flow_cms):
+    """Converts flow rate in cubic feet per second to cubic metres per second."""
+    flow_cfs = flow_cms * 35.3146662
+    return flow_cfs
